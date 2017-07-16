217cf44693cbbe990b85f193a7efb518dac5ae93
Ryan Faulhaber <faulhaberryan@gmail.com>
2017-07-15 21:09:58 -0400

    .idea removed, notes added, .gitignore updated

--- src/layouts/CharacterCreation/CharacterCreation.jsx
+++ src/layouts/CharacterCreation/CharacterCreation.jsx
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import CharacteristicSelection from '../../components/CharacteristicSelection/CharacteristicSelection';
 import SkillSelection from '../../components/SkillSelection/SkillSelection';
 
+/*
+ TODO: create constant array of character creation stages, create component for each stage, increment / decrement
+ stage based on progress
+ */
 export default class CharacterCreation extends Component {
     constructor(props) {
         super(props);
