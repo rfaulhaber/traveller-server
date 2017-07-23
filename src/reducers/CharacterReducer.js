eb8f66fb4c7c7f48c3e12a46bce94b0e96a2e25c
Ryan Faulhaber <faulhaberryan@gmail.com>
2017-07-22 20:00:04 -0400

    added redux templates

---
+++ src/reducers/CharacterReducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import CharacterCreation from './CharacterReducer';
+
+const reducers = combineReducers({
+    CharacterCreation
+});
+
+export default reducers;
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+const initialState = {};
+
+export default function CharacterCreation(state = initialState, action) {
+    return state;
+};
